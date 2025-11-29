8b207c9b5e7adcf23db954a859f1b966c2474611
Developershaha <shahajikokane9049@gmail.com>
2025-11-29 06:04:50 +0530

    added real time polymorphism code

---
+++ src/polymorphism/RealPoly.java
@@ -0,0 +1,43 @@
+package polymorphism;
+
+class Animal{
+    public void eat(){
+        System.out.println("animal is eating");
+    }
+    public void running(){
+        System.out.println("animal is running");
+    }
+}
+
+class Tiger extends Animal{
+    public void eat(){
+        System.out.println("tiger hunts and ear");
+    }
+    public void running(){
+        System.out.println("tiger run for hunts ");
+    }
+}
+class Monkey extends Animal{
+    public void eat(){
+        System.out.println("monkey steal and eats ");
+    }
+    public void running(){
+        System.out.println("monkey is running after steal");
+    }
+}
+
+class Forest{
+    public void permit(Animal animal){
+        animal.eat();
+        animal.running();
+    }
+}
+public class RealPoly {
+    public static void main(String[] args) {
+        Tiger tr=new Tiger();
+        Monkey mk=new Monkey();
+        Forest fr=new Forest();
+        fr.permit(tr);
+        fr.permit(mk);
+    }
+}
