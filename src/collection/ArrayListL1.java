ceae0d82d8a1c61f548234128a2133e1759023cf
Developershaha <shahajikokane9049@gmail.com>
2025-12-09 07:22:05 +0530

    added array list code collection

---
+++ src/collection/ArrayListL1.java
@@ -0,0 +1,18 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class ArrayListL1 {
+    public static void main(String[] args) {
+
+        ArrayList names = new ArrayList<>();
+
+        // Adding elements
+        names.add("John");
+        names.add("Alex");
+        names.add("Riya");
+
+        // Printing ArrayList
+        System.out.println(names);
+    }
+}
