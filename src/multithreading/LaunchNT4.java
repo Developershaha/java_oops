05b26c44bf2c12019ab6a860c694d92522c1f1f2
Developershaha <shahajikokane9049@gmail.com>
2025-12-20 17:10:11 +0530

    avoid concurancy using join and asynch keyword

--- src/multithreading/LaunchNT4.java
+++ src/multithreading/LaunchNT4.java
@@ -3,7 +3,7 @@ package multithreading;
 class Demo1 {
     int count;
 
-    void increament() {
+   synchronized void  increament() {
         count++;
 
     }
@@ -12,7 +12,7 @@ class Demo1 {
 }
 
 public class LaunchNT4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Demo1 d = new Demo1();
         Runnable obj1 = () -> {
             for (int i = 1; i <= 1000; i++) {
@@ -28,6 +28,8 @@ public class LaunchNT4 {
         Thread t2 = new Thread(obj2);
         t1.start();
         t2.start();
+        t1.join();
+        t2.join();
         System.out.println("Count  "+d.count);
     }
 }
