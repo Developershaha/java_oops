dc3994de8113d7541193482b38da990a7d224dc3
Developershaha <shahajikokane9049@gmail.com>
2026-08-24 23:21:20 +0530

    added selection sort algo

---
+++ src/DSA/SortingAlgo/SelectionSort.java
@@ -0,0 +1,24 @@
+package DSA.SortingAlgo;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int arr[]={7,1,5,4,3,2};
+        for(int i=0;i<arr.length-1;i++){
+            int min=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+                if(min!=i){
+                    int temp=arr[i];
+                    arr[i]=arr[min];
+                    arr[min]=temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
